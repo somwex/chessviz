d6352d75420e610a09da44024bcfe2665ddecb66
p2 <pashan210100@mail.ru>
2019-04-02 12:30:00 +0700

    Edit main.c

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
-void printdesk(char desk[8][8])
+void showdesk(char desk[8][8])
+
 {
-    int i,j;
+    int i, j;
+
     printf("\n");
+
     for (i = 7; i >= 0; i--) {
         printf("%d|", i + 1);
+
         for (j = 0; j < 8; j++) {
             printf(" %c", desk[i][j]);
         }
-    printf("\n");
+        printf("\n");
     }
-    printf("   _______________\n");
-    printf("   A B C D E F G H\n");
+    printf(" _______________\n");
+    printf(" A B C D E F G H\n");
 }
 
 int main()
 {
-	char desk[8][8] = {
- 		"rhbqkbhr",
- 		"pppppppp",
- 		"........",
- 		"........",
- 		"........",
- 		"........",
- 		"PPPPPPPP",
- 		"RHBQKBHR" };
-    printdesk(desk);
+    char desk[8][8] = {"rhbqkbhr",
+                       "pppppppp",
+                       "........",
+                       "........",
+                       "........",
+                       "........",
+                       "PPPPPPPP",
+                       "RHBQKBHR"};
+    showdesk(desk);
     return 0;
-};
+}
